22f90b3df462bf3ed2c0b7be309e8bdeee5a3839
Zackisnnob <146810732+Zackisnnob@users.noreply.github.com>
2024-07-22 20:05:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,6 +102,11 @@ with open(pkl_filename, 'rb') as file:
             st.write(round(Ypredict[0][1], 2))
 
 
+st.divider()
+Thanks for the training data obtained from Kaggle.com.
+
+
+            
 # X_test=[[v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23]]
 # st.write(X_test)
 
