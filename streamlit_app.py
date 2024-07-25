f4bea163d9b1d553eb080c45a5fd103d808a9d72
Zackisnnob <146810732+Zackisnnob@users.noreply.github.com>
2024-07-25 00:57:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,7 +197,7 @@ with open(pkl_filename, 'rb') as file:
             st.write(X_temp.T)
             st.write("The probability that one should be treated of mental health:")
             st.write(round(Ypredict[0][1], 2))
-            # X_temp.to_csv('mental_health_survey_date.csv', mode='a', index=False, header=False)
+            X_temp.to_csv('mental_health_survey_date.csv', mode='a', index=False, header=False)
 
 st.divider()
 st.write("**_This web app was developed by CYD mental health group and thanks for the public dataset from Kaggle.com._**")
