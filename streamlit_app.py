422b9923f1f4272397a753d7971a49ba426aeea3
Zackisnnob <146810732+Zackisnnob@users.noreply.github.com>
2024-07-25 01:15:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,7 +197,8 @@ with open(pkl_filename, 'rb') as file:
             st.write(X_temp.T)
             X_temp.to_csv('mental_health_survey_date.csv', mode='a', index=False, header=False)
             df = pd.read_csv('mental_health_survey_date.csv')
-            st.write(df.shape)
+            
+            st.write(f'You are the #{df.shape[0]} user. Thanks for your participation.')
             
             st.write("The probability that one should be treated of mental health:")
             st.write(round(Ypredict[0][1], 2))
